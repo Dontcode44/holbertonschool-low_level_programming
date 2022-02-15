19b57f248315e6d2393175c6b9cc6f29ade9ff01
Dontcode44 <4659@holbertonschool.com>
2022-02-15 14:42:26 -0800

    sing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - Check Holberton
+ * print_sign - 5-sing
  * @n: An input number
  * Description: Tohis function prints the sign of the number
  * Return: 1 if number is positive, o if number is 0, or
@@ -12,6 +12,7 @@ int print_sign(int n)
 
 {
 	int sing;
+	
 	if (n > 0)
 	{
 		sing = 1;
