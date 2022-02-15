d048bfedbee903d87e0bad82b3a8ee8f128d7c7b
Dontcode44 <4659@holbertonschool.com>
2022-02-15 14:53:30 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		sing = 1;
+		n = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		sing = 0;
+		n = 0;
 		_putchar('0');
 	}
 	else
 	{
-		sing = -1;
+		n = -1;
 		_putchar('-');
 	}
 
-	return (sing);
+	return (n);
 }
