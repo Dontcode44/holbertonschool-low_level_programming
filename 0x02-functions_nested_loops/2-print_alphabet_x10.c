4f8f5be2130ec3f0dfa8fa2a0c07474512eb48b0
Dontcode44 <4659@holbertonschool.com>
2022-02-15 11:30:27 -0800

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10 - Print the alphabet in lowercase 10 times
- * Description:  print_alphabet_x10
- * Return: Always 0.
+ * print_alphabet_x10 - Print the alphabet 10 times
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing
  */
 
 void print_alphabet_x10(void)
