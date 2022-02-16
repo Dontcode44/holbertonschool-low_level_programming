8f345751425d09555bdfb41e7523f427be6410e5
Dontcode44 <4659@holbertonschool.com>
2022-02-15 20:20:55 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *
- *
- *
+ * print_to_98 - check
+ * @n: variable integer
+ * Description: Print the numbers up to 98
+ * Return: Success!
  */
 void print_to_98(int n)
 {
 	if(n <= 98)
 	{
 		for (n = n; n <= 97; n++)
-			_putchar("%d, ", n);
+			printf("%d, ", n);
 	}
 	else
 		for(n = n; n > 98; n--)
-			_putchar("%d, " n);
+			printf("%d, " n);
 }
