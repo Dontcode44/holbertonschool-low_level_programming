957f96174a4d86dfd20569ee257f43e225656018
Dontcode44 <4659@holbertonschool.com>
2022-02-16 17:37:56 -0800

    first

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <time.h>
+
+/**
+ * positive_or_negative - Function that prints if integer
+ * is positive or negative
+ * Description: This function prints if the number 98 is n or p
+ * @i: Variable integer
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+
+	return (0);
+}
