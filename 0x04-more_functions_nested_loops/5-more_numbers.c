cf5190ab77aafa3ad3a29e52e52ca2d57ede41ff
Dontcode4 <hector.note0@gmail.com>
2022-02-17 21:22:38 -0500

    For...for

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - A function that checks for a digit (0 through 9).
+ * @c: An input integer
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+void more_numbers(void)
+{
+	int a, b = 0;
+
+	for (; b < 10; b++)
+	{
+		for (a = 0;a <= 14; a++)
+		{
+			if (i > 9)
+			    _putchar(i / 10 + '0');
+			    
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
