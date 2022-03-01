de0b168761d66fa82ff1247f01cca595d3f9a651
Dontcode44 <4659@holbertonschool.com>
2022-03-01 11:42:38 -0500

    srt

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,8 +3,8 @@
 
 /**
  * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
+ * @ha: an input string to search in
+ * @ne: an input string to locate into string haystack
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
