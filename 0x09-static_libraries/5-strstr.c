5306b3d32c01fd2f7ba2a25e7315b0d2ab4febfa
Dontcode44 <4659@holbertonschool.com>
2022-03-05 22:14:42 -0500

    First

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strstr -  a function that locates a substring.
+ * @ha: an input string to search in
+ * @ne: an input string to locate into string haystack
+ * Return:  a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ha = haystack;
+	char *ne = needle;
+
+	while (*ha != '\0')
+	{
+		haystack = ha;
+	        ne = needle;
+		while (*ne != '\0' && *ha == *ne)
+		{
+			ha++;
+			ne++;
+		}
+		if (*ne == '\0')
+			return (haystack);
+		ha = haystack + 1;
+	}
+	return (NULL);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * _abs - Computes the absolute value
+ * of an integer
+ * @ab: Number to check
+ * Return: Absolute value
+ */
+
+int _abs(int ab)
+
+{
+	if (ab >= 0)
+		return (ab);
+	else
+		return (ab * -1);
+}
