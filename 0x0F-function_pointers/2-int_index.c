f15d5a59b05c02c7fb114b1f2abeee5a6addd4aa
Dontcode44 <4659@holbertonschool.com>
2022-03-17 11:21:37 -0500

    fix and indent int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,19 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a;
+
 	if (array == NULL)
 		return (-1);
-	if (size <= 0)
+
+	if (size >= 0)
 		return (-1);
+
 	for (a = 0; a < size; a++)
+	{
 		if (cmp(array[a]) != 0)
+		{
 			return (a);
+		}
+	}
 	return (-1);
 }
