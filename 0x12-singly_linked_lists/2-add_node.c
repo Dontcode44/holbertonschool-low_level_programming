79f1e8b6e8c410c8e3907938678d256daec92494
Dontcode44 <4659@holbertonschool.com>
2022-03-29 18:14:27 -0500

    Add new node at the beginning of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - Function that adds new node
+ * @head: Head of the list
+ * @str: Type string variable pointer 
+ * Return: New node element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *bandido;
+	int a = 0;
+
+	bandido = malloc(sizeof(list_t));
+
+	if (bandido == NULL)
+	{
+		free(bandido);
+		return(NULL);
+	}
+	for (; str[a]; a++);
+	bandido->str = strdup[str];
+	bandido->len = a;
+	bandido->next = *head;
+	*head = bandido;
+
+	return (bandido);
+}
