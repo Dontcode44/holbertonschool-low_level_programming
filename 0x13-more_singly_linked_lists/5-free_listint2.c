c486159cb51a5bcbd15a55e344658a5288ec9665
Dontcode44 <4659@holbertonschool.com>
2022-04-01 20:14:33 -0500

    Set

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that frees a list
+ * @head: Header from list 
+ *Return: Success!
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *set;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head)
+	{
+		set = (*head)->next;
+		free(*head);
+		*head = set;
+	}
+	*head = NULL;
+}
