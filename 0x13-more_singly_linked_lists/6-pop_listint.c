55be13589e8ce03a49086a280b992beb46139a7f
Dontcode44 <4659@holbertonschool.com>
2022-04-01 23:08:23 -0500

    Pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function that deletes the head
+ * @head: Header from list
+ * Return: Always 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int node = 0;
+
+	if (*head == NULL)
+		return (0);
+
+	node = ptr->n;
+	*head = (*head)->next;
+	free(ptr);
+
+	return (node);
+}
