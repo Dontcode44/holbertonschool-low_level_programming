2cf66ed2dbe9d276380069d4faf7f9a6f5655e75
Dontcode44 <4659@holbertonschool.com>
2022-04-02 00:44:54 -0500

    Try to reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,7 +14,7 @@ listint_t *reverse_listint(listint_t **head)
 		while (*head)
 		{
 			next = (*head)->next;
-			prev = (*head)->next;
+			(*head)->next = prev;
 			prev = *head;
 			*head = next;
 		}
