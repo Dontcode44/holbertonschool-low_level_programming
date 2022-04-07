ff22670fe6e428e6391292371523d8d00932ac35
Dontcode44 <4659@holbertonschool.com>
2022-04-07 02:37:46 -0500

    Override

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rd = read(fd, buffer, letters);
 	if (rd == -1)
 		return (0);
-	wr = write(STDOUT_FILENO, buffer, rd);
+	wr = write(1, buffer, rd);
 	if (wr == -1)
 		return (0);
 	close(fd);
