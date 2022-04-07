dd8a455dbe6eb96d0976897d53d0a2f38be84fcf
Dontcode44 <4659@holbertonschool.com>
2022-04-07 12:07:23 -0500

    Fixed output error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 
 	fd = open(filename, O_WRONLY, O_CREAT, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+		text_content = "";
 	for (i = 0; text_content[i]; i++)
 		;
 	wr = write(fd, text_content, i);
