d8889f5b07c06411ea29106eb32f649be4b961e7
Dontcode44 <4659@holbertonschool.com>
2022-04-07 03:00:03 -0500

    Proof a create new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - Function that create a file
+ * @filename: Filename const type char
+ * @text_content: Text inside filename ready to read
+ * Return: Success! or 1, also -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i]; i++)
+		;
+	wr = write(fd, text_content, i);
+	if (wr == -1)
+		return (-1);
+	return (1);
+}
