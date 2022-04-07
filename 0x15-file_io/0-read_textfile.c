03e1990f5d121538da79c3036e05374ee45fb0a9
Dontcode44 <4659@holbertonschool.com>
2022-04-07 02:29:57 -0500

    Read first commit and main.h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file
+ * @letters: Size of letters
+ * @filename: Const type char
+ * Return: Success!
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t rd, wr;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	return (wr);
+}
