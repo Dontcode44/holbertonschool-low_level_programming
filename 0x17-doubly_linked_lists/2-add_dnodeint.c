a3d90632f505c73ecb06b7c3bc17f3345b653d9e
Dontcode44 <4659@holbertonschool.com>
2022-05-12 14:43:53 -0500

    add begenning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_dnodeint - Function thats adds new node beginning
+ * @head: Header of node
+ * @n: Const int
+ * Return: Success!
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->next = n;
+	new->prev = NULL;
+	new->n = n;
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (new);
+}
