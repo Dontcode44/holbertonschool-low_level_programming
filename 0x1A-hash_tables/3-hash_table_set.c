0ec3f631a171645d9df34cd3c8cc010e379bf6fd
Dontcode44 <4659@holbertonschool.com>
2022-06-06 13:31:03 -0500

    Try to do

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Create a new node in hash
+ * @key: Key
+ * @value: Value const char
+ * Return: Success!!
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int head;
+	hash_node_t *tmp;
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (ht == NULL || key == NULL || *key == NULL)
+		return (0);
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	head = key_index((unsigned char *)key, ht->size);
+	if (ht->array[head] != NULL)
+	{
+		tmp = ht->array[head];
+		while (tmp != NULL)
+		{
+			if (strcmp(temp->, new_node->key) == 0)
+				break;
+			{
+				tmp = tmp->next;
+			}
+			if (tmp == NULL)
+			{
+				new_node->next = ht->array[head];
+				ht->array[head] = new_node;
+			}
+			else
+			{
+				free(tmp->value);
+				tmp->value = strdup(new_node->value);
+				free(new_node->value);
+				free(new_node->key);
+				free(new_node);
+			}
+		}
+		else
+		{
+			new_node->next = NULL;
+			ht->array[head] = new_node;
+		}
+		return (1);
+	}
+	
+}
