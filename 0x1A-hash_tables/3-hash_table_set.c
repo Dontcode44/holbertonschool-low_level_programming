8ceeb0473eb3e68f4180df4eb2515136ffb68360
Dontcode44 <4659@holbertonschool.com>
2022-06-06 15:18:10 -0500

    Fixed variable names

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,13 +28,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[head] == NULL)
 	{
-		node = malloc(sizeof(hash_node_t *));
-		if (node == NULL)
+		hash_node = malloc(sizeof(hash_node_t *));
+		if (hash_node == NULL)
 			return (0);
 		hash_node->next = NULL;
 		hash_node->key = strdup(key);
 		hash_node->value = strdup(value);
-		ht->array[idx] = hash_node;
+		ht->array[head] = hash_node;
 	}
 	else
 	{
