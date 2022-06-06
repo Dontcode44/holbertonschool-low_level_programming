bf4bf10ac09f35867ea4236bcf02f4a714ebd874
Dontcode44 <4659@holbertonschool.com>
2022-06-06 14:55:53 -0500

    Try to do

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,44 +8,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int head;
-	hash_node_t *tmp;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	unsigned int head;
+	hash_node_t *hash_node;
 
-	if (ht == NULL || key == '\0' || *key == '\0')
+	if (ht == NULL)
 		return (0);
-	if (new_node == NULL)
+	if (key == NULL || *key == '\0')
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+
 	head = key_index((unsigned char *)key, ht->size);
 	if (ht->array[head] != NULL)
 	{
-		tmp = ht->array[head];
-		while (tmp != NULL)
+		if (strcmp(key, ht->array[head]->key) == 0)
 		{
-			if (strcmp(temp->, new_node->key) == 0)
-				break;
-			tmp = tmp->next;
+			ht->array[head]->value = strdup(value);
+			return (1);
 		}
-		if (tmp == NULL)
-		{
-			new_node->next = ht->array[head];
-			ht->array[head] = new_node;
-		}
-		else
-		{
-			free(tmp->value);
-			tmp->value = strdup(new_node->value);
-			free(new_node->value);
-			free(new_node->key);
-			free(new_node);
-		}
+	}
+
+	if (ht->array[head] == NULL)
+	{
+		node = malloc(sizeof(hash_node_t *));
+		if (node == NULL)
+			return (0);
+		hash_node->next = NULL;
+		hash_node->key = strdup(key);
+		hash_node->value = strdup(value);
+		ht->array[idx] = hash_node;
 	}
 	else
 	{
-		new_node->next = NULL;
-		ht->array[head] = new_node;
+		hash_node = malloc(sizeof(hash_node_t *));
+
+		hash_node->key = strdup(key);
+		hash_node->value = strdup(value);
+		hash_node->next = ht->array[head];
+		ht->array[head] = hash_node;
 	}
 	return (1);
 }
