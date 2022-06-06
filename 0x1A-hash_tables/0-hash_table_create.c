09e9172e196087ce2caea51637efd78dc7b9c49a
Dontcode44 <4659@holbertonschool.com>
2022-06-06 15:03:33 -0500

    Try

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *new_table = NULL;
 	unsigned long int head;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
@@ -22,5 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (head = 0; head < size; head++)
 		new_table->array[head] = NULL;
+	new_table->size = size;
+
 	return (new_table);
 }
