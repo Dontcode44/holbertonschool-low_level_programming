c65d4a5d49813043654ae6d626cb1d45d94c3edd
Dontcode44 <4659@holbertonschool.com>
2022-06-06 11:23:34 -0500

    Adds comments

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - Algoritm
+ * @str: String char
+ * Return: Success!
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
