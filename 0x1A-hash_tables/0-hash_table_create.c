71f7f18b5318cff8d90b3e209ab6b4ebf2f82988
Dontcode44 <4659@holbertonschool.com>
2022-06-06 11:19:58 -0500

    Try to do

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Function that create new hash table
+ * @size: Unsigned long int to the size
+ * Return: Success!!
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int head;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (head = 0; head < size; head++)
+		new_table->array[head] = NULL;
+	return (new_table);
+}
