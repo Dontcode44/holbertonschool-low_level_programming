de41727ce4ae32d7804e8513d2c6c61fe9b673ff
Dontcode44 <4659@holbertonschool.com>
2022-06-06 11:31:20 -0500

    Index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - Function that gives the index from key
+ * @key: Key from table
+ * @size: Unsigned long int of size
+ * Return: Success!
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL)
+		return (0);
+
+	return (hash_djb2(key) % size);
+}
