187af8fa7c4ea03ccbde57b4f7102a6fdb5fc013
Dontcode44 <4659@holbertonschool.com>
2022-06-06 23:40:20 -0500

    Hash delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Function that deletes hash table
+ *@ht: Hash table
+ *Return: Success!!
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int position = 0;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return;
+	for (position = 0; position < ht->size; position++)
+	{
+		while (ht->array[position] != NULL)
+		{
+			tmp = ht->array[position]->next;
+			free(ht->array[position]->key);
+			free(ht->array[position]->value);
+			free(ht->array[position]);
+			ht->array[position] = tmp;
+		}
+		free(ht->array[position]);
+	}
+	free(ht->array);
+	free(ht);
+}
