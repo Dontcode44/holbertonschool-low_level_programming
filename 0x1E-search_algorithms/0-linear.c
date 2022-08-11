cbcb63e2a8010180bec5eb56d8e7472a3cf1adb6
Dontcode44 <4659@holbertonschool.com>
2022-08-10 23:33:24 -0500

    Linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,12 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL)
         return (-1);
 
-    for (int i = 0; i < (int)size; i++)
+    for (i = 0; i < (int)size; i++)
     {
         printf("Value checked array[%d] = [%d]\n", i, array[i])
 
         if (array[i] == value)
             return (i);
-        
     }
     return (-1);
 }
